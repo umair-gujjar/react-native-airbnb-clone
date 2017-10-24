4e2edfb8f1d74c3576149f9d2c3c144f68a935a5
Andy Andrei <web@imandy.ie>
2017-10-24 21:41:46 +0100

    Add no saved homes screen

--- src/containers/SavedContainer.js
+++ src/containers/SavedContainer.js
@@ -6,10 +6,13 @@
 
 import React, { Component } from 'react';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import colors from '../styles/colors';
 
 import {
   View,
   Text,
+  ScrollView,
+  TouchableHighlight,
   StyleSheet,
 } from 'react-native';
 
@@ -25,10 +28,27 @@ export default class ExploreContainer extends Component {
     ),
   };
 
+  nothingSavedMessage() {
+    return(
+      <View>
+        <ScrollView style={styles.scrollView}>
+          <Text style={styles.heading}>Saved</Text>
+          <Text style={styles.description}>Not every day is filled with adventures, but you can start planning for the next one.</Text>
+          <Text style={styles.description}>Tap the heart on any home to start saving your favorites here.</Text>
+        </ScrollView>
+        <View style={styles.footer}>
+          <TouchableHighlight style={styles.findHomesBtn}>
+            <Text style={styles.findHomesBtnText}>Find homes</Text>
+          </TouchableHighlight>
+        </View>
+      </View>
+    );
+  }
+
   render() {
     return (
       <View style={styles.wrapper}>
-        <Text style={styles.welcomeText}>Saved Tab</Text>
+        {this.nothingSavedMessage()}
       </View>
     );
   }
@@ -36,7 +56,48 @@ export default class ExploreContainer extends Component {
 
 const styles = StyleSheet.create({
   wrapper: {
-    display: 'flex',
-    padding: 50,
+    flex: 1,
+    backgroundColor: colors.white,
   },
+  heading: {
+    fontSize: 30,
+    fontWeight: '600',
+    marginBottom: 40,
+    color: colors.gray02,
+    marginTop: 70,
+    paddingLeft: 20,
+    paddingRight: 20,
+  },
+  description: {
+    fontSize: 16,
+    lineHeight: 24,
+    color: colors.gray02,
+    paddingLeft: 20,
+    paddingRight: 20,
+  },
+  scrollView: {
+    height: '100%',
+  },
+  footer: {
+    position: 'absolute',
+    width: '100%',
+    height: 80,
+    bottom: 0,
+    borderTopWidth: 1,
+    borderTopColor: colors.gray05,
+    paddingLeft: 20,
+    paddingRight: 20,
+  },
+  findHomesBtn: {
+    paddingTop: 15,
+    paddingBottom: 15,
+    marginTop: 16,
+    borderRadius: 3,
+    backgroundColor: colors.pink,
+  },
+  findHomesBtnText: {
+    color: colors.white,
+    textAlign: 'center',
+    fontWeight: '600',
+  }
 });
